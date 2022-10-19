9596e5a7a15068eae61d2800a42f12891b99b57e
nyraa <112930946+nyraa@users.noreply.github.com~>
2022-10-19 22:05:00 +0800

    add domHelper

---
+++ docs/main.js
@@ -0,0 +1,34 @@
+const {
+    Signal,
+    ShowIf,
+    QueryRouter,
+    div,
+    nav,
+    ul,
+    li,
+    input,
+    button,
+    table,
+    thead,
+    tbody,
+    tr,
+    th,
+    td,
+    iframe,
+    p,
+    span,
+    h1,
+    a,
+    br,
+    text,
+    img,
+    svg,
+    label,
+    TextState,
+    State,
+    ClassList
+} = require('../NCKUpp/domHelper');
+(async function main()
+{
+
+})();
