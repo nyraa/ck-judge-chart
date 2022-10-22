7817dd4a47a3c62f1e9b27b3ba64574042e94fbf
nyraa <112930946+nyraa@users.noreply.github.com>
2022-10-23 01:28:34 +0800

    add sorting function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useState} from "react";
 
 import Navigator from "./containers/Navigator";
 
@@ -8,18 +9,42 @@ import useJudgeData from './hooks/useJudgeData';
 
 function App() {
     const { problems, homeworkSets, stats, ready: dataReady } = useJudgeData();
+    const [sortingMode, setSortingMode] = useState(0);
+    const sortingTypeEvent = baseId => {
+        return () => {
+            if(sortingMode === baseId) setSortingMode(baseId + 1);
+            else setSortingMode(baseId);
+        };
+    };
+    const sortingTypeIndicator = baseId => {
+        if(sortingMode === baseId) return "sorting-mode sorting-mode--0";
+        else if(sortingMode === baseId + 1) return "sorting-mode sorting-mode--1";
+        else return "sorting-mode";
+    }
+    const sortingTypes = [
+        () => -1,
+        () => 0,
+        (a, b) => b.submit - a.submit,
+        (a, b) => a.submit - b.submit,
+        (a, b) => b.ac - a.ac,
+        (a, b) => a.ac - b.ac,
+        (a, b) => b.ac_rate - a.ac_rate,
+        (a, b) => a.ac_rate - b.ac_rate,
+        (a, b) => b.non_ac - a.non_ac,
+        (a, b) => a.non_ac - b.non_ac
+    ];
     return (
         <div className="App">
             <Navigator />
             <div className="title-bar">
-                <div className="problem-name-title">Problems</div>
-                <div className="submit-counts-title">Submit</div>
-                <div className="ac-counts-title">AC</div>
-                <div className="ac-rate-title">AC Rate</div>
-                <div className="non-ac-counts-title">Non-AC</div>
+                <div className={`problem-name-title ${sortingTypeIndicator(0)}`} onClick={sortingTypeEvent(0)}>Problems</div>
+                <div className={`submit-counts-title ${sortingTypeIndicator(2)}`} onClick={sortingTypeEvent(2)}>Submit</div>
+                <div className={`ac-counts-title ${sortingTypeIndicator(4)}`} onClick={sortingTypeEvent(4)}>AC</div>
+                <div className={`ac-rate-title ${sortingTypeIndicator(6)}`} onClick={sortingTypeEvent(6)}>AC Rate</div>
+                <div className={`non-ac-counts-title ${sortingTypeIndicator(8)}`} onClick={sortingTypeEvent(8)}>Non-AC</div>
                 <div className="history-title">History</div>
             </div>
-            <DataList arg_problems={problems} arg_homeworkSets={homeworkSets} arg_stats={stats} arg_dataReady={dataReady} />
+            <DataList arg_problems={problems} arg_homeworkSets={homeworkSets} arg_stats={stats} arg_dataReady={dataReady} arg_sortingFunc={sortingTypes[sortingMode]} />
         </div>
     );
 }
--- src/containers/DataList.js
+++ src/containers/DataList.js
@@ -1,18 +1,37 @@
 import "./DataList.css";
 
-function DataList({arg_problems, arg_stats, arg_homeworkSets, arg_dataReady})
+function DataList({arg_problems, arg_stats, arg_homeworkSets, arg_dataReady, arg_sortingFunc})
 {
+    if(!arg_dataReady)
+    {
+        return (<div className="loading-message">
+            Loading...
+        </div>);
+    }
+    let row_data = arg_problems.map((problem) => {
+        return {
+            title: problem.title,
+            id: problem.id,
+            submit: arg_stats[problem.id].at(-1)[1],
+            ac: arg_stats[problem.id].at(-1)[2],
+            ac_rate: (arg_stats[problem.id].at(-1)[2] * 100 / arg_stats[problem.id].at(-1)[1]).toFixed(2),
+            non_ac: arg_stats[problem.id].at(-1)[1] - arg_stats[problem.id].at(-1)[2]
+        };
+    });
+    row_data.sort(arg_sortingFunc);
     return (<div className="data-list-root">
-        {arg_dataReady
-        && <>{arg_problems.map((problem) => (<div className="problem-row" key={problem.id}>
+        {row_data.map((data) => (<div className="problem-row" key={data.id}>
             <div className="problem-icon"></div>
-            <div className="problem-name">{problem.title}</div>
-            <div className="submit-counts">{arg_stats[problem.id].at(-1)[1]}</div>
-            <div className="ac-counts">{arg_stats[problem.id].at(-1)[2]}</div>
-            <div className="ac-rate">{(arg_stats[problem.id].at(-1)[2] * 100 / arg_stats[problem.id].at(-1)[1]).toFixed(2)}%</div>
-            <div className="non-ac-counts">{arg_stats[problem.id].at(-1)[1] - arg_stats[problem.id].at(-1)[2]}</div>
+            <div className="problem-name">
+                {data.title}
+                <a href={`https://ckj.imslab.org/#/problems/${data.id}`} target="_blank">[*]</a>
+            </div>
+            <div className="submit-counts">{data.submit}</div>
+            <div className="ac-counts">{data.ac}</div>
+            <div className="ac-rate">{data.ac_rate}%</div>
+            <div className="non-ac-counts">{data.non_ac}</div>
             <canvas className="history-canvas" height="50px" width="200px"></canvas>
-        </div>))}</>}
+        </div>))}
     </div>);
 }
 
